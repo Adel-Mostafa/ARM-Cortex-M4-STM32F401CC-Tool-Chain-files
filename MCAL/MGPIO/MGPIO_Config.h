7969b07d7e848a21727c95d112a99b5fd53bce78
Adel Mostafa Abdelhameed <152022546+Adel-Mostafa@users.noreply.github.com>
2024-05-22 23:00:29 +0300

    Create MGPIO_Config.h

---
+++ MCAL/MGPIO/MGPIO_Config.h
@@ -0,0 +1,12 @@
+/*
+ *  MGPIO_Config.h
+ *  Created on: April 22, 2024
+ *  Author: ADEL MOSTAFA ABDELHAMEED
+ */
+
+#ifndef MGPIO_CONFIG_H_
+#define MGPIO_CONFIG_H_
+
+
+
+#endif /* MGPIO_CONFIG_H_ */
